08a68e88deb65901d6c9ce8f5a35f1232faf6200
Garrett S <gsmith17@uccs.edu>
2024-03-30 09:18:52 -0600

    Added portfolio_app and made some changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,13 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+# ...
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'Django.contrib.staticfiles',
+'DG-portfolio_app', 
 ]
+
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
